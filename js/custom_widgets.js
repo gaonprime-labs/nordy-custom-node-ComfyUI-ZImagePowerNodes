7a51722dca86c095b28023fe9fbcc2ba866871a1
Martin Rizzo <martinrizzo@gmail.com>
2026-08-09 19:11:04 -0300

    refactor: Centralize custom widget initialization in "custom_widgets.js"
    The commit moves all custom widget initialization logic into a single
    `js/custom_widgets.js` file. While the actual code for each widget
    remains in its respective file within the `/custom_widgets` directory,
    this change centralizes the binding with the ComfyUI framework.
    
    This reorganization makes the project easier to maintain by preventing
    initialization logic from being scattered across multiple files.

--- js/custom_widgets.js
+++ js/custom_widgets.js
@@ -11,28 +11,123 @@
  *_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 */
 import { app }                          from "../../../scripts/app.js";
-import { addSeparatorWidget }           from "./custom_widgets/separator_widget.js";
-import { addVisualStyleGalleryWidget }  from "./custom_widgets/ui_styles.js";
-import { addColorPaletteGalleryWidget } from "./custom_widgets/ui_palettes.js";
+import { GalleryWidget }                from "./custom_widgets/gallery_widget.js";
+import { SeparatorWidget }              from "./custom_widgets/separator_widget.js";
 import { addStyleGalleryButton }        from "./custom_widgets/style_gallery_button.js";
 import { CustomStylesComboController }  from "./custom_widgets/custom_styles_combo.js";
+import { StyleWidgetDelegate  , requireVisualStyleGalleryDialog  } from "./custom_widgets/ui_styles.js";
+import { PaletteWidgetDelegate, requireColorPaletteGalleryDialog } from "./custom_widgets/ui_palettes.js";
 const ENABLED = true;
 
+
 /**
- * Creates and adds a Combo widget that automatically synchronizes custom styles
- * from the user's style definitions.
+ * Adds a separator widget to the node.
  *
- * This function is called directly by the ComfyUI framework whenever a node
- * containing this widget is instantiated.
+ * The data[1] (options) object supports the following properties:
+ *  - `mode`      {string}: Determines the visual appearance. Available modes:
+ *                          "spacer" (empty vertical padding), "divider" (standard line),
+ *                          "dotted" (dashed line), or "bold" (thick line).
+ *  - `color`     {string}: A CSS color string to define the line color.
+ *  - `height`    {number}: The total vertical space allocated for the widget in pixels.
+ *  - `thickness` {number}: The line weight in pixels.
  *
- * @param {LGraphNode} node - The node instance where the widget must be added
- * @param {string}     name - Unique identifier for the widget (not used for value serialization)
- * @param {Array}      data - Configuration array where:
- *                                  - [0] = Widget type name
- *                                  - [1] = Object containing the widget configuration.
- * @param {object}     _app - The ComfyApp instance (not used in this implementation)
+ * @param {object} node - The node instance where the widget will be attached.
+ * @param {string} name - The name identifier for the widget.
+ * @param {Array}  data - Configuration array where:
+ *                        - [0] = widget type name
+ *                        - [1] = object containing the optional configurations.
+ * @param {object} _app - The ComfyApp instance.
+ *
  * @returns {{ widget: object }}
- *     Object containing the created widget instance.
+ *     An object containing the added separator widget instance.
+ */
+function _addSeparator(node, name, data, _app) {
+    const type    = data[0];
+    const options = data[1] || {};
+    const widget  = node.addCustomWidget( new SeparatorWidget(type, name, options) );
+    return { widget: widget };
+}
+
+
+/**
+ * Adds a "Style Selector" widget that utilizes a gallery dialog to select the style.
+ *
+ * @param {object} node - The node instance where the widget will be attached.
+ * @param {string} name - The name identifier for the widget.
+ * @param {Array}  data - Configuration array where:
+ *                        - [0] = widget type name
+ *                        - [1] = object containing the optional configurations.
+ * @param {object} _app - The ComfyApp instance.
+ *
+ * @returns {{ widget: object }}
+ *     An object containing the added style selector widget.
+ */
+function _addStyleSelector(node, name, data, _app) {
+    const type          = data[0];
+    const options       = data[1] || {};
+    const endpoint      = options.endpoint   || "";
+    const imagesURL     = options.images_url || "";
+    const dialogOptions = options.dialog || {};
+    const widgetDelegate = new StyleWidgetDelegate(endpoint, imagesURL);
+    let widget = new GalleryWidget(type, node, name, options, widgetDelegate, (widget) =>
+    {
+        // launch dialog and update widget value
+        const styleDialog  = requireVisualStyleGalleryDialog(endpoint, imagesURL);
+        const currentStyle = widget.value;
+        styleDialog.launch( dialogOptions, currentStyle, (selectedStyle) => {
+            widget.forceUpdate( selectedStyle );
+        });
+    });
+    widget = node.addCustomWidget( widget );
+    return { widget: widget };
+}
+
+
+/**
+ * Adds a "Palette Selector" widget that utilizes a gallery dialog to select a palette.
+ *
+ * @param {object} node - The node instance where the widget will be attached.
+ * @param {string} name - The name identifier for the widget.
+ * @param {Array}  data - Configuration array where:
+ *                        - [0] = widget type name
+ *                        - [1] = object containing the optional configurations.
+ * @param {object} _app - The ComfyApp instance.
+ *
+ * @returns {{ widget: object }}
+ *     An object containing the added palette selector widget.
+ */
+function _addPaletteSelector(node, name, data, _app) {
+    const type           = data[0];
+    const options        = data[1] || {};
+    const endpoint       = options.endpoint || "";
+    const dialog_options = options.dialog || {};
+    let widget = new GalleryWidget(type, node, name, options, new PaletteWidgetDelegate(endpoint), (widget) =>
+    {
+        // launch dialog and update widget value
+        const paletteDialog  = requireColorPaletteGalleryDialog(endpoint);
+        const currentPalette = widget.value;
+        paletteDialog.launch( dialog_options, currentPalette, (selectedPalette) => {
+            widget.forceUpdate( selectedPalette );
+        });
+    });
+    widget = node.addCustomWidget( widget );
+    return { widget: widget };
+}
+
+
+/**
+ * Adds a Combo widget that automatically synchronizes custom styles from
+ * the user's style definitions.
+ *
+ * @param {object} node - The node instance where the widget will be attached.
+ * @param {string} name - The name identifier for the widget.
+ * @param {Array}  data - Configuration array where:
+ *                        - [0] = widget type name
+ *                        - [1] = object containing the optional configurations.
+ * @param {object} _app - The ComfyApp instance.
+ *
+ * @returns {{ widget: object }}
+ *     An object containing the added combo box widget.
  */
 function _addCustomStyleSelector(node, name, data, _app) {
     const _type         = data[0];
@@ -45,6 +140,7 @@ function _addCustomStyleSelector(node, name, data, _app) {
     node.zipnCustStylesController = new CustomStylesComboController(widget, node, widgetOptions.options);
     return { widget: widget };
 }
+
 
 //#=========================================================================#
 //#////////////////////////// REGISTER EXTENSION ///////////////////////////#
@@ -63,9 +159,9 @@ app.registerExtension({
     getCustomWidgets() {
         if( !ENABLED ) return {};
         return {
-            "ZIPN_SEPARATOR"            : addSeparatorWidget,
-            "ZIPN_STYLE_SELECTOR"       : addVisualStyleGalleryWidget,
-            "ZIPN_PALETTE_SELECTOR"     : addColorPaletteGalleryWidget,
+            "ZIPN_SEPARATOR"            : _addSeparator,
+            "ZIPN_PALETTE_SELECTOR"     : _addPaletteSelector,
+            "ZIPN_STYLE_SELECTOR"       : _addStyleSelector,
             "ZIPN_CUSTOM_STYLE_SELECTOR": _addCustomStyleSelector,
 
             // [DEPRECATED]
