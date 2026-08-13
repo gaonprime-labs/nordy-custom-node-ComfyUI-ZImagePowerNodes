67981191f0ff04d637156986e9b0b2183f38dfe6
Martin Rizzo <martinrizzo@gmail.com>
2026-08-12 21:07:33 -0300

    feat: Replace old "Image Labeler" with a new extensible version
    The commit deprecates the original "Image Labeler" node in favor of
    a new implementation, internally named `ImageLabeler2`. The primary
    difference at this stage is the addition of an enable/disable toggle
    switch. This new class serves as the foundation for upcoming
    expansions to the node's functionality.

--- __init__.py
+++ __init__.py
@@ -194,6 +194,9 @@ class ZImagePowerNodesExtension(ComfyExtension):
         from .nodes.deprecated_nodes.zsampler_turbo_2_laboratory import ZSamplerTurbo2Laboratory
         _register_node( ZSamplerTurbo2Laboratory, nodes, subcategory )
 
+        from .nodes.deprecated_nodes.image_labeler_1 import ImageLabeler
+        _register_node( ImageLabeler, nodes, subcategory )
+
 
         #--[ __experimental ]--------------------
         subcategory = "__experimental"
@@ -223,8 +226,8 @@ class ZImagePowerNodesExtension(ComfyExtension):
         #--[ utils ]-----------------------------
         subcategory = "utils"
 
-        from .nodes.image_labeler import ImageLabeler
-        _register_node( ImageLabeler, nodes, subcategory )
+        from .nodes.image_labeler_2 import ImageLabeler2
+        _register_node( ImageLabeler2, nodes, subcategory )
 
         from .nodes.image_grid_builder import ImageGridBuilder
         _register_node( ImageGridBuilder, nodes, subcategory )
